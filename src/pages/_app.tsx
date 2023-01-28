239ebedc9335eb326261081f50c1c5dd960ecb14
Rafael Borges <rafael.borges@luizalabs.com>
2023-01-28 10:08:39 -0300

    initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,41 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+
+import theme from "@/config/theme";
+import createEmotionCache from "@/config/createEmotionCache";
+
+import MiniDrawer from "@/components/layout/MiniDrawer";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface IApp extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App(props: IApp) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>my organizer app</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MiniDrawer>
+          <Component {...pageProps} />
+        </MiniDrawer>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
