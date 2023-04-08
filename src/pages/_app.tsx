3695666bb722c878c8717e2fadba40dacd45650a
Rafael Borges <rafael.borges@luizalabs.com>
2023-04-08 17:29:19 -0300

    config react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
-import '@/styles/globals.css'
-
-// import type { AppProps } from 'next/app'
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
+import { queryClient } from '@/service/query-client'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import type { ReactElement, ReactNode } from 'react'
+import { QueryClientProvider } from 'react-query'
+
+import '@/styles/globals.css'
+
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -17,9 +15,14 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />)
+    </QueryClientProvider>
+  )
 }
