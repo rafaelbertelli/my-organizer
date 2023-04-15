4729f812ddf02f0fea0e0a248d808749731d091a
Rafael Borges <rafael.borges@luizalabs.com>
2023-04-15 14:28:55 -0300

    install packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/flowbite/**/*.js",
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
